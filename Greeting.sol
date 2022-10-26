0d0404bbc51c96932f85269324a3b15e076fc7a2
Hameed Saberi <95750655+hameedg@users.noreply.github.com>
2022-10-27 00:32:04 +0530

    Create Greeting.sol

---
+++ Greeting.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.17;
+contract Greeting{
+
+
+string public name;
+string public greetingPrefix="Hello ";
+constructor(string memory initialName){
+    name=initialName;
+}
+function setName(string memory newName) public{
+    name=newName;
+
+}
+function getGreeting() public view returns(string memory){
+    return string(abi.encodePacked(greetingPrefix,name));
+}
+
+}
